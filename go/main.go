da866ee569b612dd020467ac2044c33f709c3111
Bruno Belarte <bruno.belarte@thoughtworks.com>
2023-07-19 15:28:25 +0200

    add main.go

---
+++ go/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/belarte-tw/copilot-experiments/cmd"
+
+// generate the main function and execute
+// the root command from the cmd package
+func main() {
+	cmd.Execute()
+}
