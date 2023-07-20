51aeeac9773e8081795d9a3b2e4d35494038f137
Bruno Belarte <bruno.belarte@thoughtworks.com>
2023-07-20 12:10:52 +0200

    more thorough tests for server

--- go/server/server_test.go
+++ go/server/server_test.go
@@ -13,10 +13,23 @@ import (
 
 func TestServerStart(t *testing.T) {
 	go server.Start("127.0.0.1:8080")
-
 	time.Sleep(time.Microsecond * 100)
 
-	resp, err := http.Get("http://127.0.0.1:8080/health")
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	var tests = []struct {
+		url      string
+		expected int
+	}{
+		{"http://127.0.0.1:8080/health", http.StatusOK},
+		{"http://127.0.0.1:8080/actors/nm0000102", http.StatusOK},
+		{"http://127.0.0.1:8080/movies/tt0109830", http.StatusOK},
+		{"http://127.0.0.1:8080/invalid", http.StatusNotFound},
+	}
+
+	for _, test := range tests {
+		t.Run(test.url, func(t *testing.T) {
+			resp, err := http.Get(test.url)
+			assert.NoError(t, err)
+			assert.Equal(t, test.expected, resp.StatusCode)
+		})
+	}
 }
